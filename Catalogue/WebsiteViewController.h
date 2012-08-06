93b21719033c81aa69f85e144a6b744f68d7a740
lsujstans7 <jstans7@lsu.edu>
2012-08-06 15:49:34 -0500

    end of class

---
+++ Catalogue/WebsiteViewController.h
@@ -0,0 +1,15 @@
+//
+//  WebsiteViewController.h
+//  Catalogue
+//
+//  Created by iOS Camp on 8/6/12.
+//  Copyright (c) 2012 kcfancher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebsiteViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
---
+++ Catalogue/WebsiteViewController.m
@@ -0,0 +1,48 @@
+//
+//  WebsiteViewController.m
+//  Catalogue
+//
+//  Created by iOS Camp on 8/6/12.
+//  Copyright (c) 2012 kcfancher. All rights reserved.
+//
+
+#import "WebsiteViewController.h"
+
+@interface WebsiteViewController ()
+
+@end
+
+@implementation WebsiteViewController
+@synthesize webView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    NSURL *url = [NSURL URLWithString:@"http://www.apple.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
+
+- (void)viewDidUnload
+{
+    [self setWebView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
